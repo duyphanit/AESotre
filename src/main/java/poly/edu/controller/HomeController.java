da6e4ab757a1ff095845b036bfb883a98d16c71a
duyphanit <duypnps38998@gmail.com>
2025-05-31 02:57:56 +0700

    code FE

--- src/main/java/poly/edu/controller/HomeController.java
+++ src/main/java/poly/edu/controller/HomeController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class indexcontroller {
-	@RequestMapping("/home")
-	public String indexcontroller() {
-		return "index";
+public class HomeController {
+	
+	@RequestMapping({"/", "/home", "/home/index"})
+	public String home() {
+		return "index_layout";
 	}
+	
 }
